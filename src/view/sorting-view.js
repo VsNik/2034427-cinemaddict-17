dafca1f3b2eda85d7c63f325febb0a96dd9c1c97
VsNik <v.joxow@gmail.com>
2022-05-20 16:43:33 +0300

    module6-task1

--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,22 +1,31 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import {SortType} from '../constant.js';
 
-const createSortingTemplate = () => `
+const createSortingTemplate = (sortType) => `
   <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${sortType === SortType.DEFAULT ? 'sort__button--active' : ''}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${sortType === SortType.DATE ? 'sort__button--active' : ''}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${sortType === SortType.RATING ? 'sort__button--active' : ''}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`;
 
-export default class SortingView extends AbstractView {
+export default class SortingView extends AbstractStatefulView {
+
+  constructor() {
+    super();
+    this._state = {...this._state, currentSortType: SortType.DEFAULT};
+  }
 
   get template() {
-    return createSortingTemplate();
+    return createSortingTemplate(this._state.currentSortType);
   }
 
   setSortTypeChangeHandler = (callback) => {
     this._callback.sortTypeChange = callback;
     this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  _restoreHandlers = () => {
+    this.setSortTypeChangeHandler(this._callback.sortTypeChange);
   };
 
   #sortTypeChangeHandler = (evt) => {
@@ -26,6 +35,7 @@ export default class SortingView extends AbstractView {
 
     evt.preventDefault();
     this._callback.sortTypeChange(evt.target.dataset.sortType);
+    this.updateElement({...this._state, currentSortType: evt.target.dataset.sortType});
   };
 }
 
